9cec9f42af5879947cf58dd0f43f711c2b9e89ab
Cayden Lund <caydenlund@gmail.com>
2025-02-03 12:19:21 -0700

    Add encode/decode traits

--- src/encode/error.rs
+++ src/encode/error.rs
@@ -1,8 +1,10 @@
 use std::error::Error;
 use std::fmt::Display;
 
-#[derive(Debug, Clone)]
-pub enum EncodeError {}
+#[derive(Debug)]
+pub enum EncodeError {
+    WriteError(std::io::Error),
+}
 
 impl Display for EncodeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
--- src/encode/mod.rs
+++ src/encode/mod.rs
@@ -1,2 +1,13 @@
 mod error;
 pub use error::*;
+
+use std::io::Write;
+
+pub trait Encode {
+    fn encoding(&self) -> Result<Vec<u8>, EncodeError>;
+
+    fn insert_encoding<T: Write>(&self, dst: &mut T) -> Result<(), EncodeError> {
+        dst.write_all(&self.encoding()?)
+            .map_err(EncodeError::WriteError)
+    }
+}
